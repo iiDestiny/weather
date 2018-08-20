ab97597caf133b138d8e3ac59ba79ac2d9dabc18
iiDestiny <iidestiny@vip.qq.com>
2018-08-20 09:29:52 +0000

    Apply fixes from StyleCI

--- src/Exceptions/HttpException.php
+++ src/Exceptions/HttpException.php
@@ -1,9 +1,15 @@
 <?php
 
-namespace IiDestiny\Weather\Exceptions;
+/*
+ * This file is part of the iidestiny/weather.
+ *
+ * (c) 罗彦 <iidestiny@vip.qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ */
 
+namespace IiDestiny\Weather\Exceptions;
 
 class HttpException extends Exception
 {
-
 }
